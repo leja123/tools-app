809e9f271acb7f49daf29167d6972c9136519b5a
Leja Podobnik <leja123@users.noreply.github.com>
2026-03-14 10:46:42 +0100

    basic setup

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="sl">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Tools app</title>
+    </head>
+    <body>
+        <h2>Tools app</h2>
+        <div id="login">
+            <h4>Login</h4><br>
+            <form action="">
+                Username<input type="text"></input><br>
+                Password<input type="text"></input><br>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+        <div id="register">
+            <h4>Register</h4><br>
+            <form action="">
+                Username<input type="text"></input><br>
+                Password<input type="text"></input><br>
+                Repeat password<input type="text"></input><br>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    </body>
+</html>
+
